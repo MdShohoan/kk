d5cd87b222def84b17b8aa2f48f52c9546032647
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2023-11-18 14:57:44 +0600

    completed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
 function Footer() {
     return (
-        <section>
-            <footer>
-                footer
+        <section className='bg-[#494949] pb-6 pt-16'>
+            <footer className="text-xs font-normal text-white text-center">
+                <span className="block mb-4">স্বত্ব © ২০২৩ কালের কণ্ঠ</span>
+                <span className="block mb-2">প্লট-৩৭১/এ, ব্লক-ডি, বসুন্ধরা, বারিধারা, ঢাকা-১২২৯</span>
+                <span className="block leading-5">পিএবিএক্স : ০৯৬১২১২০০০০, ৫৫০৩৬৮০৩-০৬ <br/>
+                    বার্তা বিভাগ ফ্যাক্স : ৮৪৩২৩৬৮-৬৯ <br/>
+                    বিজ্ঞাপন ফোন : ৮৪৩২০৪৮</span>
             </footer>
         </section>
     )
