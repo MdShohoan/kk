ffcc79a0d0bdfb94004cacef7c49e5d5e76099f2
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2023-11-21 15:56:13 +0600

    updated video section font color, play icon and hover design

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import thumbnail1 from '../../assets/videoThumbnail/thumbnail1.png'
 import thumbnail2 from '../../assets/videoThumbnail/thumbnail2.png'
 import Title from './../common/Title/Title';
-import { FaPlay } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-
+import { GoPlay } from "react-icons/go";
 
 function Video() {
+    const [hoverElement, setHoverElement] = useState(null)
+
+    const handleVideoHover = (element) => {
+        setHoverElement(element.id)
+    }
+
     const data = [
-        { title: 'নির্বাচন কি সঠিক হবে ?', thumbnail: thumbnail1 },
-        { title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
-        { title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
-        { title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
-        { title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
-        { title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
+        { id: 1, title: 'নির্বাচন কি সঠিক হবে ?', thumbnail: thumbnail1 },
+        { id: 2, title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
+        { id: 3, title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
+        { id: 4, title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
+        { id: 5, title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
+        { id: 6, title: 'সরকার পোশাকশিল্প ধ্বংসের নীলনকশা বাস্তবায়ন করছে : রিজভী', thumbnail: thumbnail2 },
     ]
 
     return (
@@ -21,9 +26,13 @@ function Video() {
                 <Title text={'ভিডিও'} underline='type1' />
                 <div className='mb-5 md:m-10' />
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                    <div className='h-full md:flex flex-col'>
+                    <div
+                        className='h-full md:flex flex-col cursor-pointer'
+                        onMouseEnter={() => handleVideoHover(data[0])}
+                        onMouseLeave={() => setHoverElement(null)}
+                    >
                         <div
-                            className='md:h-full mb-3 rounded flex justify-center items-center'
+                            className='md:h-full rounded flex justify-center items-center relative'
                             style={{
                                 backgroundImage: `url(${data[0]?.thumbnail})`,
                                 backgroundRepeat: 'no-repeat',
@@ -32,15 +41,27 @@ function Video() {
                             }}
                         >
                             <span className='inline-block py-16 md:p-0'>
-                                <FaYoutube color='#FF0000' size={68} />
+                                <GoPlay
+                                    color={hoverElement === 1 ? 'red' : 'white'} size={48}
+                                    style={{ transition: 'all 300ms' }}
+                                />
                             </span>
                         </div>
-                        <h6 className='text-primary font-extrabold text-2xl'>{data[0]?.title}</h6>
+                        <h6
+                            className={`text-${hoverElement === 1 ? 'primary-contrast' : 'primary'} font-extrabold text-2xl pt-3 transition-all duration-300`}
+                        >
+                            {data[0]?.title}
+                        </h6>
                     </div>
                     <div className='flex flex-col gap-4'>
                         {
                             data?.slice(1, 5).map((info, i) => (
-                                <div key={i} className='flex gap-2 border-b border-gray4 pb-2'>
+                                <div
+                                    key={i}
+                                    className='flex gap-2 border-b border-gray4 pb-2 cursor-pointer'
+                                    onMouseEnter={() => handleVideoHover(info)}
+                                    onMouseLeave={() => setHoverElement(null)}
+                                >
                                     <div
                                         className='h-[50px] w-[104px] flex justify-center items-center rounded'
                                         style={{
@@ -50,9 +71,14 @@ function Video() {
                                             backgroundPosition: 'center',
                                         }}
                                     >
-                                        <FaPlay color='white' size={16} />
+                                        <GoPlay
+                                            color={hoverElement === info?.id ? 'red' : 'white'}
+                                            style={{ transition: 'all 300ms' }}
+                                            size={24}
+
+                                        />
                                     </div>
-                                    <h6 className='text-sm text-gray2 font-normal'>{info?.title}</h6>
+                                    <h6 className={`text-sm text-${hoverElement === info?.id ? 'blue-500' : '[#666]'} font-normal transition-all duration-300`}>{info?.title}</h6>
                                 </div>
                             ))
                         }
