1e136d51b36e11924aeaf4f69f835f17bd3385ad
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2023-11-21 12:58:43 +0600

    updated style for hover state in quick link section

--- src/components/QuickLinks/QuickLinks.jsx
+++ src/components/QuickLinks/QuickLinks.jsx
@@ -1,31 +1,51 @@
-import { MdOutlineEventSeat  } from "react-icons/md";
+import { MdOutlineEventSeat } from "react-icons/md";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { IoMdGitCompare } from "react-icons/io";
 import { MdOutlineAddReaction } from "react-icons/md";
 import { colors } from "../../theme";
+import { useState } from "react";
 
 
 function QuickLinks() {
+    const [hoverLink, setHoverLink] = useState(null)
+
+    const setSubtitleHoverColor = (link)=>{
+        return hoverLink === link.id ? 'text-blue-500' : 'text-gray1'
+    }
+
     const quickLinks = [
-        { title: 'আসন', subtitle: 'জেনে নিন আপনার আসন সম্পর্কে', icon: MdOutlineEventSeat },
-        { title: 'ম্যাপ ', subtitle: 'নির্বাচনের তথ্য জানতে ম্যাপ উপর ক্লিক করুন', icon: FaMapMarkerAlt},
-        { title: 'ফলাফল', subtitle: ' নির্বাচনের  ফলাফল', icon: MdOutlineAddReaction},
-        { title: 'হার-জিত', subtitle: 'দেখুন কে কোথায় জিতেছিল', icon: IoMdGitCompare },
+        { id: 'seat', title: 'আসন', subtitle: 'জেনে নিন আপনার আসন সম্পর্কে', icon: MdOutlineEventSeat },
+        { id: 'map', title: 'ম্যাপ ', subtitle: 'নির্বাচনের তথ্য জানতে ম্যাপ উপর ক্লিক করুন', icon: FaMapMarkerAlt },
+        { id: 'result', title: 'ফলাফল', subtitle: ' নির্বাচনের  ফলাফল', icon: MdOutlineAddReaction },
+        { id: 'win-lose', title: 'হার-জিত', subtitle: 'দেখুন কে কোথায় জিতেছিল', icon: IoMdGitCompare },
     ]
 
     return (
-        <section className="mb-12 md:mb-24 bg-primary-light p-8">
+        <section className="mb-12 md:mb-24 bg-primary-light px-0 py-6 md:p-8">
             <div className="container mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-16">
                     {
                         quickLinks.map((link, i) => {
-                            let Icon = link?.icon || MdOutlineEventSeat 
+                            let Icon = link?.icon || MdOutlineEventSeat
                             return (
-                                <div key={i} className="flex justify-start lg:justify-center items-center gap-3">
-                                    <div><Icon size={32} style={{color: colors?.primary?.default}}/></div>
+                                <div
+                                    onMouseEnter={() => setHoverLink(link?.id)}
+                                    onMouseLeave={() => setHoverLink(null)}
+                                    key={i}
+                                    className="flex justify-start lg:justify-center items-center gap-3 cursor-pointer"
+                                >
                                     <div>
-                                        <span className="block text-primary font-bold text-base mb-1">{link.title}</span>
-                                        <span className="block text-xs text-gray1 font-normal">{link.subtitle}</span>
+                                        <Icon
+                                            size={32}
+                                            style={{
+                                                color: hoverLink === link.id ? colors.primary.contrast : colors?.primary?.default,
+                                                transition:'all 300ms'
+                                            }}
+                                        />
+                                    </div>
+                                    <div>
+                                        <span className={`transition-all duration-300 block text-${hoverLink === link.id ? 'primary-contrast' : 'primary'} font-bold text-base mb-1`}>{link.title}</span>
+                                        <span className={`transition-all duration-300 block text-xs ${setSubtitleHoverColor(link)} font-normal`}>{link.subtitle}</span>
                                     </div>
                                 </div>
                             )
