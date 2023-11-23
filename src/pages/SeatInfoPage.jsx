cc4cd0824c9a2e7a819615b04652d12efd6156dd
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2023-11-23 14:06:26 +0600

    completed election result section

--- src/pages/SeatInfoPage.jsx
+++ src/pages/SeatInfoPage.jsx
@@ -9,6 +9,7 @@ import total_candidate from '../assets/election/total-seat.png'
 import male_voter from '../assets/election/male-voter.png'
 import female_voter from '../assets/election/female-voter.png'
 import Title from '../components/common/Title/Title'
+import jatipartySymbol from '../assets/partyLogo/JatioyoPartyLogo.webp'
 import { ScrollRestoration } from 'react-router-dom'
 
 function SeatInfoPage() {
@@ -49,21 +50,24 @@ function SeatInfoPage() {
 
     return (
         <>
-             <ScrollRestoration/>
+            <ScrollRestoration />
             <Layout>
                 <section className='mb-12 md:mb-24'>
                     <div className='container'>
                         <div className='mb-8'>
                             <DivisionNavigator />
                         </div>
-
+                    </div>
+                </section>
+                <section className='mb-12 md:mb-24'>
+                    <div className='container'>
                         <div className='mb-4'>
                             <span className='inline-block rounded-lg bg-primary-light text-2xl py-[6px] px-4 mb-2'>ঢাকা-১</span>
                             <div className='bg-gray1 h-[2px]' />
                         </div>
 
                         {/* ------------Seat information counter start---------- */}
-                        <div className='grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-7 mb-12'>
+                        <div className='grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-7'>
                             {
                                 seatData.map((singleData, i) => (
                                     <Card key={i} type='type1' data={singleData} />
@@ -71,11 +75,14 @@ function SeatInfoPage() {
                             }
                         </div>
                         {/* -----------Seat information counter end------- */}
-
-                        {/* -----------Candidates information start-------- */}
+                    </div>
+                </section>
+                <section className='mb-12 md:mb-24'>
+                    <div className='container'>
                         <div className="mb-8">
                             <Title text={'প্রার্থী'} underline="type2" />
                         </div>
+                        {/* -----------Candidates information start-------- */}
                         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                             {
                                 candidates.map((candidate, i) => (
@@ -91,6 +98,46 @@ function SeatInfoPage() {
                             }
                         </div>
                         {/* ------------Candidates information end-------------- */}
+                    </div>
+                </section>
+                <section className='mb-12 md:mb-24'>
+                    <div className="container">
+                        <div className="mb-8">
+                            <Title text={'নির্বাচন ১৯৯১ থেকে ২০১৮'} underline="type2" />
+                        </div>
+                        <div className='grid grid-cols-1 gap-4 md:gap-0 md:grid-cols-2 md:[&>*:nth-child(odd)]:border-r'>
+                            {
+                                ['', '', '', '', '', ''].map((_, i) => (
+                                    <div key={i} className='border md:border-0 md:border-t p-3 md:p-4 lg:p-8 xl:p-16'>
+                                        <div className='bg-primary-light h-36 w-36 rounded-full mx-auto mb-8 text-center flex justify-center items-center flex-col'>
+                                            <span className='block mb-2 text-2xl font-bold text-primary'>১২ ম</span>
+                                            <span className='block text-gray-700 font-bold text-sm'>
+                                                সংসদ নির্বাচন <br /> ২০১৪
+                                            </span>
+                                        </div>
+                                        <div className='flex justify-between mb-8'>
+                                            <div>
+                                                <span className='block text-xl font-bold text-gray-700 mb-1'>৩,৭৫,৯৫৮</span>
+                                                <span className='block text-gray1 text-sm font-semibold'>মোট ভোটার</span>
+                                            </div>
+                                            <div>
+                                                <span className='block text-right text-xl font-bold mb-1 text-gray-700'>১৭৭</span>
+                                                <span className='block text-right text-gray1 text-sm font-semibold'>মোট কেন্দ্র</span>
+                                            </div>
+                                        </div>
+                                        <div className='flex flex-col md:flex-row items-center justify-center md:justify-between bg-primary-light p-8 rounded-md gap-6 md:gap-0'>
+                                            <img className='w-14 md:w-[75px]' src={jatipartySymbol} alt="" />
+                                            <div className='text-center md:text-right'>
+                                                <div className='text-gray-700 font-bold mb-2'>সালমা ইসলাম</div>
+                                                <div className='text-gray-700 text-sm mb-1'>দল: জাতীয় পার্টি </div>
+                                                <div className='text-gray-700 text-sm mb-1'>প্রতীক: লাঙল </div>
+                                                <div className='text-gray-700 text-sm mb-1'>প্রাপ্ত ভোট: ৫৩,৩৪১</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </section>
             </Layout>
