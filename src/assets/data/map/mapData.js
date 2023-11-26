e62d9619738bc2e9ffd8e1483185ea2858756171
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2023-11-26 12:47:15 +0600

    completed map for dhaka division except dhaka district

--- src/assets/data/map/mapData.js
+++ src/assets/data/map/mapData.js
@@ -8,19 +8,28 @@ import narayanganj  from './narayanganj'
 import rajbari from './rajbari'
 import faridpur from './faridpur'
 import gopalganj from './gopalganj'
+import madaripur from './madaripur '
+import shariatpur from './shariatpur'
 
 const mapData = [
     tangail,
     kishoreganj,
     manikganj,
     munshiganj,
-
+    //dhaka
     gazipur,
     narsingdi,
     narayanganj,
     rajbari,
     faridpur,
     gopalganj,
+    madaripur,
+    shariatpur,
+    //End of Dhaka Division
+
+    
+
+
 ]
 
 export default mapData
--- src/components/common/SeatMap/SeatMap.jsx
+++ src/components/common/SeatMap/SeatMap.jsx
@@ -18,13 +18,23 @@ function SeatMap() {
                 xmlSpace="preserve"
             >
                 {
-                    mapData[9]?.data?.map((seat) => (
+                    mapData[11]?.data?.map((seat) => (
                         <Link to="/" key={seat?.id}>
+                            {
+                                seat.seatNo === '222' | '223' && (
+                                    <path
+                                        id={`seat_${seat?.id}`}
+                                        className=" fill-[#9db3f6] stroke-white stroke-1 hover:fill-[#ffd740] transition-all duration-1000"
+                                        style={{ strokeMiterlimit: 10 }}
+                                        d={seat?.points}
+                                    />
+                                )
+                            }
                             <polyline
                                 id={`seat_${seat?.id}`}
                                 className=" fill-[#9db3f6] stroke-white stroke-1 hover:fill-[#ffd740] transition-all duration-1000"
                                 style={{ strokeMiterlimit: 10 }}
-                                points= {seat?.points}
+                                points={seat?.points}
                             />
                             <text transform={seat?.transform} className="fill-black text-base font-bold pointer-events-none">
                                 {seat?.text}
