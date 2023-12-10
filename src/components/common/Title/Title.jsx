eb37dd7709fc5e10cfce9d656d19ec60310a0826
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2023-12-10 13:34:26 +0600

    fixed title underline

--- src/components/common/Title/Title.jsx
+++ src/components/common/Title/Title.jsx
@@ -7,11 +7,12 @@ function Title({ text, underline = 'type2' }) {
             // >
             //     {text || 'No title'}
             // </h4>
+
             <h4
                 className={
-                    `text-center relative py-4 px-3 pb-3 text-lg sm:text-2xl font-bold text-gray-700
+                    `text-center w-fit relative py-4 px-5 pb-3 text-lg sm:text-2xl font-bold text-gray-700 mx-auto
                     before:w-[12px] before:h-[12px] before:bg-primary before:absolute before:bottom-[-3px] before:left-[50%] before:-translate-x-2/4 before:rotate-45
-                    after:w-[25%] after:h-[3px] after:absolute after:bottom-[1px] after:left-[50%] after:-translate-x-1/2 after:bg-gradient-to-r after:from-primary after:to-primary after:via-transparent`
+                    after:w-[100%] after:h-[3px] after:absolute after:bottom-[1px] after:left-[50%] after:-translate-x-1/2 after:bg-gradient-to-r after:from-primary after:to-primary after:via-transparent`
                 }
             >
                 {text || 'No title'}
