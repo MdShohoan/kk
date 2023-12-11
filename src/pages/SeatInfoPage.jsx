ade5a79ea29cb8f3d7f0fc28a6d726becdf36ece
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2023-12-11 16:05:27 +0600

    fixed candidate card column for mobile version

--- src/pages/SeatInfoPage.jsx
+++ src/pages/SeatInfoPage.jsx
@@ -3,7 +3,6 @@ import DivisionNavigator from '../components/common/DivisionNavigator/DivisionNa
 import Layout from '../components/common/Layout/Layout'
 
 import candidat1 from '../assets/candidatePhoto/asaduzzaman.webp'
-
 import total_voter from '../assets/election/total-voter.png'
 import total_candidate from '../assets/election/total-seat.png'
 import male_voter from '../assets/election/male-voter.png'
@@ -11,6 +10,10 @@ import female_voter from '../assets/election/female-voter.png'
 import Title from '../components/common/Title/Title'
 import jatipartySymbol from '../assets/partyLogo/JatioyoPartyLogo.webp'
 import { ScrollRestoration } from 'react-router-dom'
+
+import seatsData from '../assets/data/seats.json'
+
+console.log('===seat', seatsData)
 
 function SeatInfoPage() {
 
@@ -60,7 +63,7 @@ function SeatInfoPage() {
                         </div>
 
                         {/* ------------Seat information counter start---------- */}
-                        <div className='grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-7'>
+                        <div className='grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-8'>
                             {
                                 seatData.map((singleData, i) => (
                                     <Card key={i} type='type1' data={singleData} />
@@ -70,17 +73,21 @@ function SeatInfoPage() {
                         {/* -----------Seat information counter end------- */}
                     </div>
                 </section>
+
                 <section className='mb-12 md:mb-24'>
                     <div className='container'>
                         <div className="mb-8">
                             <Title text={'প্রার্থী'} underline="type1" />
                         </div>
                         {/* -----------Candidates information start-------- */}
-                        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+                        <div className="grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-8">
                             {
                                 candidates.map((candidate, i) => (
                                     <div key={i} className="text-center cursor-pointer bg-[#b9b9b969] py-5 rounded-xl">
-                                        <img src={candidate?.photo} className="bg-[#b9b9b969] mx-auto block border-4 border-[#fff] rounded-full shadow-[0_0_5px_rgba(0,0,0,0.6)]" />
+                                        <img
+                                            src={candidate?.photo}
+                                            className="bg-[#b9b9b969] w-[100px] md:w-[130px] mx-auto block border-4 border-[#fff] rounded-full shadow-[0_0_5px_rgba(0,0,0,0.6)]"
+                                        />
                                         <div className="mt-5">
                                             <h2 className="text-lg font-bold text-[#547cf5] mb-2">{candidate?.name}</h2>
                                             <span className="block text-sm mb-[2px] font-normal text-[#000000cc]"><span className="font-semibold">দল:</span> {candidate?.party}</span>
