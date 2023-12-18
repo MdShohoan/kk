afb16c07316664b1a6e1f66fa828954939183273
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2023-12-18 12:32:35 +0600

    updated

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -2,6 +2,7 @@ import Section from "../common/Section/Section"
 import BangladeshMap from '../../components/common/BangladeshMap/BangladeshMap'
 import SeatNavigationMap from '../../components/common/SeatNavigationMap/SeatNavigationMap'
 import Title from "../common/Title/Title"
+import UnofficialResult from "../UnofficialResult/UnofficialResult"
 
 function Maps() {
     return (
@@ -10,22 +11,20 @@ function Maps() {
                 {/* <Title>
                     নির্বাচনের তথ্য জানতে মানচিত্রের উপর ক্লিক করুন
                 </Title> */}
-                <Title>
-                    নির্বাচনের তথ্য জানতে জেলার উপর ক্লিক করুন
-                </Title>
-                {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                    <div className="border rounded">
-                        <div className="text-center py-4 font-bold text-gray-700">জেলা</div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    <div className="border rounded pt-4">
+                        <Title>
+                            নির্বাচনের তথ্য জানতে জেলার উপর ক্লিক করুন
+                        </Title>
                         <BangladeshMap />
                     </div>
-                    <div className="border rounded">
-                        <div className="text-center py-4 font-bold text-gray-700">আসন</div>
-                        <SeatNavigationMap />
+                    <div className="border rounded pt-4">
+                        <UnofficialResult/>
                     </div>
-                </div> */}
-                <div className=" mx-auto max-w-2xl">
-                    <BangladeshMap />
                 </div>
+                {/* <div className=" mx-auto max-w-2xl">
+                    <BangladeshMap />
+                </div> */}
             </div>
         </Section>
     )
