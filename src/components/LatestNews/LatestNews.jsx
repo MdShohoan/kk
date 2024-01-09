20a75028b83c07182d5917f7fa6eee360045d3e5
Arifur Rahaman <ariful.rahman@EWMGLBD.LOCAL>
2024-01-09 12:01:16 +0600

    added more button in latest news section

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -6,9 +6,12 @@ import Section from "../common/Section/Section";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import cn from "../../lib/cn";
+import { TfiMore } from "react-icons/tfi";
+import { colors } from "../../theme";
+import { useNavigate } from "react-router-dom";
 
 export default function LatestNews() {
-
+    const navigate = useNavigate()
     const [stories, setStories] = useState([])
     const [loading, setLoading] = useState(false)
 
@@ -40,30 +43,45 @@ export default function LatestNews() {
                     নির্বাচনের সর্বশেষ খবর
                 </Title>
                 {
-                    loading ? (<Spinner className={'h-[300px]'}/>) : (
+                    loading ? (<Spinner className={'h-[300px]'} />) : (
                         <>
-                            {/* ----News Card Start (visible in big screen)---- */}
-                            <div className="hidden md:grid grid-cols-4 gap-y-4">
-                                {
-                                    stories?.slice(0, 12).map((story, i) => (
-                                        <div key={story?.n_id} className='border-t pt-4'>
-                                            <Card story={story} className={cn(`font-bold border-r px-5 pb-6`, (i + 1) % 4 === 0 && 'border-r-0 pr-0', (i + 1) === 1 && 'pl-0', (i + 1) === 5 && 'pl-0')} />
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                            {/* ----News Card End (visible in big screen)---- */}
+                            <div className=" mb-6 md:mb-9">
+                                {/* ----News Card Start (visible in big screen)---- */}
+                                <div className="hidden md:grid grid-cols-4 gap-y-4">
+                                    {
+                                        stories?.slice(0, 12).map((story, i) => (
+                                            <div key={story?.n_id} className='border-t pt-4'>
+                                                <Card story={story} className={cn(`font-bold border-r px-5 pb-6`, (i + 1) % 4 === 0 && 'border-r-0 pr-0', (i + 1) === 1 && 'pl-0', (i + 1) === 5 && 'pl-0')} />
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                                {/* ----News Card End (visible in big screen)---- */}
 
-                            {/* ----News List Start (visible in mobile screen)---- */}
-                            <div className='flex md:hidden flex-col gap-4 '>
-                                <Card story={stories[0]} className={'mb-1 pb-1 sm:pb-0 sm:border-b-0 font-bold leading-[22px] text-2xl md:text-lg'} />
-                                {
-                                    stories?.slice(1, 12).map((story) => (
-                                        <List key={story.n_id} story={story} className={cn('last:border-b-0')}/>
-                                    ))
-                                }
+                                {/* ----News List Start (visible in mobile screen)---- */}
+                                <div className='flex md:hidden flex-col gap-4 '>
+                                    <Card story={stories[0]} className={'mb-1 pb-1 sm:pb-0 sm:border-b-0 font-bold leading-[22px] text-2xl md:text-lg'} />
+                                    {
+                                        stories?.slice(1, 12).map((story) => (
+                                            <List key={story.n_id} story={story} className={cn('last:border-b-0')} />
+                                        ))
+                                    }
+                                </div>
+                                {/* ----News List end (visible in mobile screen)---- */}
                             </div>
-                            {/* ----News List end (visible in mobile screen)---- */}
+                            <div className='w-fit mx-auto'>
+                                <button
+                                    onClick={() => navigate('/news')}
+                                    className={
+                                        cn(
+                                            `inline-flex items-center gap-1 border border-primary text-primary-light py-1 px-4 rounded cursor-pointer bg-primary transit text-base`,
+
+                                        )
+                                    }>
+                                    আরো
+                                    <TfiMore size={20} color={colors.primary.light} />
+                                </button>
+                            </div>
                         </>
                     )
                 }
